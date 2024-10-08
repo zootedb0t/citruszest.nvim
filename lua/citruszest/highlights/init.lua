efdf4fdfd98c2dd58d8527c748e0f69c1209923c
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-10-08 11:45:37 +0100

    feat: set Inclue to whtei

--- lua/citruszest/highlights/init.lua
+++ lua/citruszest/highlights/init.lua
@@ -101,7 +101,7 @@ M.theme = function(C, O)
     Exception = { fg = C.blue }, --  try, catch, throw
 
     PreProc = { fg = C.bright_green }, -- (preferred) generic Preprocessor
-    Include = { fg = C.blue }, --  preprocessor #include
+    Include = { fg = C.white }, --  preprocessor #include
     Define = { link = "PreProc" }, -- preprocessor #define
     Macro = { fg = C.violet }, -- same as Define
     PreCondit = { link = "PreProc" }, -- preprocessor #if, #else, #endif, etc.
