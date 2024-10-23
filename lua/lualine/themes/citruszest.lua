e88d81a199ad6c61e7a88b4c77242558ff7a4f0f
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-10-23 11:13:06 +0100

    feat: lua line to lime green

--- lua/lualine/themes/citruszest.lua
+++ lua/lualine/themes/citruszest.lua
@@ -2,6 +2,7 @@ local colors = {
   black = "#232323",
   red = "#FF5454",
   green = "#1AFFA3",
+  lime_green = "#9fc131",
   yellow = "#FFD700",
   orange = "#FF7431",
   blue = "#00BFFF",
@@ -11,7 +12,7 @@ local colors = {
 
 return {
   normal = {
-    a = { bg = colors.green, fg = colors.black },
+    a = { bg = colors.lime_green, fg = colors.black },
     b = { bg = colors.orange, fg = colors.black },
     c = { bg = colors.black, fg = colors.white },
   },
