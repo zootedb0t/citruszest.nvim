b018b747436c43493cd6b606f4ca7f8dba32afe7
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-10-24 09:50:57 +0100

    feat: lua line bold

--- lua/lualine/themes/citruszest.lua
+++ lua/lualine/themes/citruszest.lua
@@ -13,20 +13,20 @@ local colors = {
 
 return {
   normal = {
-    a = { bg = colors.lime_green, fg = colors.black },
-    b = { bg = colors.orange, fg = colors.black },
-    c = { bg = colors.black, fg = colors.white },
+    a = { bg = colors.lime_green, fg = colors.black, gui = "bold" },
+    b = { bg = colors.orange, fg = colors.black, gui = "bold" },
+    c = { bg = colors.black, fg = colors.white, gui = "bold" },
   },
   insert = {
-    a = { bg = colors.blue, fg = colors.black },
+    a = { bg = colors.blue, fg = colors.black, gui = "bold" },
   },
   visual = {
-    a = { bg = colors.violet, fg = colors.black },
+    a = { bg = colors.violet, fg = colors.black, gui = "bold" },
   },
   replace = {
-    a = { bg = colors.black, fg = colors.yellow },
+    a = { bg = colors.black, fg = colors.yellow, gui = "bold" },
   },
   command = {
-    a = { bg = colors.orange, fg = colors.black },
+    a = { bg = colors.orange, fg = colors.black, gui = "bold" },
   },
 }
